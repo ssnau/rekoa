b2d6362471a7f750fd76e3876bcdbcadc52ef812
korige <korige@gmail.com>
2016-05-31 23:46:44 +0800

    update example

--- example/controller/greet.js
+++ example/controller/greet.js
@@ -1,5 +1,13 @@
 module.exports = [
   {
+    url: '/',
+    controller: function* (response) {
+      response.json({
+        message: "hello, i am index"
+      });
+    }
+  },
+  {
     url: '/greet',
     controller: function* (response) {
       response.json({
